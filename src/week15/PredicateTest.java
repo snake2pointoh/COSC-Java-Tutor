b7f71b8658b2a22cf1416710463927b17b586216
snake2pointoh <45461838+snake2pointoh@users.noreply.github.com>
2024-02-03 11:34:17 -0600

    prep for week 15

---
+++ src/week15/PredicateTest.java
@@ -0,0 +1,17 @@
+package src.week15;
+
+import java.util.function.Predicate;
+
+public class PredicateTest {
+
+    // predicate
+    // function
+    // consumer
+    // provider
+
+    // biPredicate
+    // biFunction
+    // biConsumer
+
+    // construct function classes using lambda
+}
